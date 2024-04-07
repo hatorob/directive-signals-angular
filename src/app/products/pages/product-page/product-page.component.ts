a5fa53afa2d03218e3115415273e8336c95dbb85
Hector Toro <alejotoro94@hotmail.com>
2024-04-07 09:48:21 -0500

    Se deja funcionando el formulario reactivo de forma tradicional, pero con diferencia de que se injecta FormBuilder de forma diferente

--- src/app/products/pages/product-page/product-page.component.ts
+++ src/app/products/pages/product-page/product-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'product-page',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class ProductPageComponent {
+
+  private fb = inject( FormBuilder );
+  //constructor( private fb: FormBuilder ) {}
+  public color:string = 'green';
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(6), Validators.email]]
+  })
+
+
+  public changeColor = (): void => {
+    this.color = '#xxxxxx'.replace(/x/g, y=>(Math.random()*16|0).toString(16));
+  }
 
 }
