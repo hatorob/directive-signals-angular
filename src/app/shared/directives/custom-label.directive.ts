a80b4a98cc97f656c3320384490f01f3fcbd5826
Hector Toro <alejotoro94@hotmail.com>
2024-04-07 10:07:20 -0500

    Agregando nuestra primera directiva custom label

---
+++ src/app/shared/directives/custom-label.directive.ts
@@ -0,0 +1,20 @@
+import { Directive, ElementRef, OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[customLabel]'
+})
+export class CustomLabelDirective implements OnInit {
+
+  private htmlElement?: ElementRef<HTMLElement>;
+
+  constructor( private el: ElementRef<HTMLElement>) {
+    this.htmlElement = el;
+
+    this.htmlElement.nativeElement.innerHTML = "Hola Mundo!";
+  }
+
+  ngOnInit(): void {
+    console.log("Directiva - NgOnInit");
+  }
+
+}
