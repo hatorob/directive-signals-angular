8a046dc49090e10a1526651ebd1d580e565e1aab
Hector Toro <alejotoro94@hotmail.com>
2024-04-07 16:37:47 -0500

    Actualizar parte de los estados de la mutación de mi usuario

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { User } from '../../interfaces/user-request,interface';
 
 @Component({
   templateUrl: './properties-page.component.html',
@@ -6,9 +7,47 @@ import { Component } from '@angular/core';
 })
 export class PropertiesPageComponent {
 
+  public user = signal<User>({
+    id: 2,
+    email: "janet.weaver@reqres.in",
+    first_name: "Janet",
+    last_name: "Weaver",
+    avatar: "https://reqres.in/img/faces/2-image.jpg"
+  });
 
-  onFieldUpdate( field: string, value: string ) {
+  onFieldUpdate( field: keyof User, value: string ) {
     console.log(field,value);
+    //! una forma de conmutarlo
+    /* this.user.set({
+      ...this.user(),
+      [field]: value
+    }) */
+
+    //! Otra forma
+    /* this.user.update( (current) => ({
+      ...this.user(),
+      [field]: value
+    })) */
+
+    //! Otra forma
+    this.user.update( current => {
+      switch( field ) {
+        case 'email':
+          current.email = value;
+          break;
+        case 'first_name':
+          current.first_name = value;
+          break;
+        case 'last_name':
+          current.last_name = value;
+          break;
+        default:
+          return current;
+      }
+      return current;
+    })
+
+
   }
 
 
